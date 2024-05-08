0463f1a543c00eda75aa62f27f5591c17aadd1fe
Yash Shinde <yashshinde337@gmail.com>
2024-05-08 11:04:25 +0530

    Revert "added font style, fixed bug"
    This reverts commit 3ec44234a1cb0d40deb235e8e269e03a0ac8a227.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,6 @@ import dummyImg from '../assets/dummyImg.jpeg'
 import { useEffect } from "react";
 import { addToCart, addToWishList, setProducts } from "../redux/actions/productActions";
 import { dummyData } from "../assets/dummyData/data";
-import LazyLoad from "react-lazyload";
 
 
 function Product() {
@@ -17,26 +16,23 @@ function Product() {
   }, [])
 
   const renderList = products.map((product) => {
-    const { id, name, price, image, category, inStock } = product;
+    const { id, name, price, image, category } = product;
     return (<div key={id}>
       <div className="box-border w-[300px] h-[470px] rounded-lg shadow-xl shadow-gray-400 transition duration-300 ease-in  hover:transition hover:duration-300 hover:ease-out">
 
         <div className="flex justify-center h-[270px]">
-          <LazyLoad height={270}>
-            <img src={image?.src || dummyImg} alt={image?.alt} className="w-[278px] rounded-t-lg" onError={(e) => e.target.src = dummyImg} />
-          </LazyLoad>
+          <img src={image?.src || dummyImg} alt={image?.alt} className="w-[278px] rounded-t-lg" onError={(e) => e.target.src = dummyImg} />
         </div>
         <div className="flex flex-col bg-slate-100 h-[200px] rounded-br-lg rounded-bl-lg px-3 justify-start gap-y-[2px] relative">
           <h2 className="text-[18px]  text-pretty">{name}</h2>
           <h3 className="text-[20px] font-bold absolute top-14">$ {price + ".00"}</h3>
           <Link to={`/product/${id}`}>
             <button
-              className="absolute top-24 w-[270px] border-2 border-black h-10 rounded transition duration-500 ease-in font-bold hover:transition hover:duration-500 hover:ease-out hover:bg-stone-900 hover:text-white"
+              className="absolute top-24 w-[270px] border-2 border-black h-10 rounded transition duration-500 ease-in  hover:transition hover:duration-500 hover:ease-out hover:bg-stone-900 hover:text-white"
             >View Product</button>
           </Link>
           <button
-            className={inStock ? "absolute top-36 bg-yellow-400 text-white w-32 h-10 rounded hover:bg-yellow-300 transition duration-500 ease-in hover:transition hover:duration-500 hover:ease-out" : "absolute top-36 bg-white text-red-600 w-32 h-10 rounded disabled:cursor-not-allowed border-2 border-red-600"}
-            disabled={!inStock}
+            className="absolute top-36 bg-yellow-400 text-white w-32 h-10 rounded hover:bg-yellow-300 transition duration-500 ease-in hover:transition hover:duration-500 hover:ease-out"
             onClick={() => dispatch(addToCart({
               id: id,
               name: name,
@@ -46,7 +42,7 @@ function Product() {
               quantity: 1,
               image: image?.src || dummyImg,
             }))}
-          >{inStock ? "Add To Cart" : "Out of Stock"}</button>
+          >Add To Cart</button>
           <button
             className="absolute top-36 right-5 text-yellow-500 border-solid border-2 border-yellow-400 w-32 h-10 rounded transition duration-500 ease-in hover:bg-yellow-400 hover:text-white hover:transition hover:duration-500 hover:ease-out hover:shadow-lg"
             onClick={() => dispatch(addToWishList({
--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -13,7 +13,7 @@ function WishList() {
     return (<>
         {wishlist.length > 0 ?
             (<div className="w-full flex flex-col items-center mb-10">
-                <h1 className="font-semibold text-4xl m-10 font-kanit">Your Wishlist</h1>
+                <h1 className="font-semibold text-4xl m-10">Your Wishlist</h1>
                 <div className="w-[70%] flex flex-wrap justify-center gap-8 mx-auto my-5">
                     {wishlist.map((item) => (
                         <div key={item.id}>
@@ -22,17 +22,17 @@ function WishList() {
                                     <img src={item.image} alt={item.name} onError={(e) => e.target.src = dummyImg} className="w-[278px] rounded-t-lg" />
                                 </div>
                                 <div className="flex flex-col bg-slate-100 h-[200px] rounded-br-lg rounded-bl-lg px-3 justify-start gap-y-[2px] relative">
-                                    <h2 className="text-pretty text-[18px] font-kanit">{item.name}</h2>
-                                    <h3 className="font-kanit">$ {item.price + ".00"}</h3>
+                                    <h2 className="text-pretty text-[18px]">{item.name}</h2>
+                                    <h3>$ {item.price + ".00"}</h3>
                                     <Link to={`/product/${item.id}`}>
                                         <button
-                                            className="absolute font-kanit top-24 w-[270px] border-2 border-black h-10 rounded transition duration-300 ease-in hover:bg-stone-900 hover:text-white hover:transition hover:duration-300 hover:ease-out"
+                                            className="absolute top-24 w-[270px] border-2 border-black h-10 rounded transition duration-300 ease-in hover:bg-stone-900 hover:text-white hover:transition hover:duration-300 hover:ease-out"
                                         >
                                             View Product
                                         </button>
                                     </Link>
                                     <button
-                                        className="absolute font-kanit top-36 bg-yellow-400 text-white w-32 h-10 rounded hover:bg-yellow-300 transition duration-500 ease-in hover:transition hover:duration-500 hover:ease-out"
+                                        className="absolute top-36 bg-yellow-400 text-white w-32 h-10 rounded hover:bg-yellow-300 transition duration-500 ease-in hover:transition hover:duration-500 hover:ease-out"
                                         onClick={() => dispatch(addToCart({
                                             id: item.id,
                                             name: item.name,
@@ -46,7 +46,7 @@ function WishList() {
                                         Add To Cart
                                     </button>
                                     <button
-                                        className="absolute font-kanit top-36 right-5 text-red-500 border-solid border-2 border-red-500 w-32 h-10 rounded transition duration-500 ease-in hover:bg-red-500 hover:text-white hover:transition hover:duration-500 hover:ease-out hover:shadow-lg"
+                                        className="absolute top-36 right-5 text-red-500 border-solid border-2 border-red-500 w-32 h-10 rounded transition duration-500 ease-in hover:bg-red-500 hover:text-white hover:transition hover:duration-500 hover:ease-out hover:shadow-lg"
                                         onClick={() => dispatch(removeFromWishlist(item))}
                                     >Remove Item</button>
                                 </div>
@@ -57,7 +57,7 @@ function WishList() {
             </div>) :
             (<div className="flex flex-col items-center justify-center h-[650px]">
                 <FaRegHeart className="text-[350px] text-gray-400" />
-                <h1 className="text-3xl font-bold font-kanit">No Items In Your Wishlist</h1>
+                <h1 className="text-3xl font-bold">No Items In Your Wishlist</h1>
             </div>
             )}
     </>);
